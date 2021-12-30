c646862577d695e2882cefd426301118c69a2c5c
nissrine saraireh <nissrine.saraireh@gmail.com>
2021-12-30 12:08:10 +0100

    created dictionary-app as react-app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          open source code by nightmare.child on <a>github</a>
+          <code>src/App.js</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
