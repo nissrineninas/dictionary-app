083f6e59a50f8e6027007a7e10fe2037c9d1b808
nissrine saraireh <nissrine.saraireh@gmail.com>
2022-01-08 17:37:44 +0100

    added phonetics component

---
+++ src/Phonetics.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Phonetics.css";
+
+export default function Phonetics(props) {
+  //add this later
+  //   function playAudio(event) {
+  //     event.preventDefault();
+
+  //   }
+  return (
+    <div className="phonetics">
+      <strong>Phonetics</strong>
+      {props.phonetics.map(function (phonetics, index) {
+        return (
+          <span key={index}>
+            <a href={phonetics.audio} target="_blank">
+              listen{" "}
+            </a>
+            <span id="synonyms"> {phonetics.text} </span>
+          </span>
+        );
+      })}
+    </div>
+  );
+}
--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,16 @@
 import React from "react";
+import Phonetics from "./Phonetics";
 import Meanings from "./Meanings";
 
 export default function Results(props) {
   console.log(props.results);
+  console.log(props.phonetics);
+
   if (props.results) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+        <Phonetics phonetics={props.results.phonetics} />
         {props.results.meanings.map(function (getAllMeanings, index) {
           return (
             <div key={index}>
