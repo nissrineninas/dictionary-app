d38df82f9d967a90616a89ef871a858fd4c0f86f
nissrine saraireh <nissrine.saraireh@gmail.com>
2022-01-09 19:44:26 +0100

    added images

---
+++ src/Photos.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Photos(props) {
+  console.log(props.pics);
+  if (props.pics) {
+    return (
+      <div className="container photos">
+        <div className="row">
+          {props.pics.photos.map(function (photo, index) {
+            return (
+              <div key={index} className="col-5">
+                <img className="img-fluid" src={photo.src.landscape} alt="" />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
--- src/SearchSubmit.js
+++ src/SearchSubmit.js
@@ -3,10 +3,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./SearchSubmit.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function SearchSubmit(props) {
   let [word, setWord] = useState(props.word);
   let [wordData, setWordData] = useState(null);
+  let [photos, setPhotos] = useState(null);
   let [conectToApi, setConnectToAPI] = useState(true);
 
   connectApi();
@@ -21,21 +23,37 @@ export default function SearchSubmit(props) {
     event.preventDefault();
     setWord(event.target.value);
   }
+
+  function handlePexelResponse(response) {
+    console.log(`phots response----------->`);
+    console.log(response.data);
+
+    setPhotos(response.data);
+    console.log(photos);
+  }
   function connectApi() {
     if (!conectToApi) {
       console.log("do not connect to API");
       return;
     }
     setConnectToAPI(false);
-    console.log("connect to API");
     //https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
     axios.get(apiUrl).then(getDefiniton);
     console.log(apiUrl);
+    // https://www.pexels.com/api/documentation/
+    const pexelsKey =
+      "563492ad6f917000010000015784ef031c2b402db410858805e8bff1";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${word}&per_page=10`;
+    console.log(`phots URL ${pexelsApiUrl}`);
+    axios
+      .get(pexelsApiUrl, { headers: { Authorization: `Bearer ${pexelsKey}` } })
+      .then(handlePexelResponse);
+
     function getDefiniton(response) {
-      console.log(response.data[0]);
+      // console.log(response.data[0]);
       setWordData(response.data[0]);
-      console.log({ wordData });
+      // console.log({ wordData });
     }
   }
 
@@ -57,12 +75,14 @@ export default function SearchSubmit(props) {
             />
             <span> </span>
             <input className="search-btn-submit" type="submit" value="Search" />
-            {/*add phrases section*/}
+            <div className="white-space"></div>
+            <Photos pics={photos} />
           </div>
           <div className="vr p-1 opacity-100"></div>
           <div className="col-md-6 auto border-left pl-2 search-form-right">
             <div className="landing-page-right-submit">
               <h1>dictionary</h1>
+
               <div className="definition-box-submit">
                 <Results results={wordData} />
               </div>
